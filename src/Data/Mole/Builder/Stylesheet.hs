d08cc1ffde62c23db58b31901e1a930172f2d2fa
Tomas Carnecky <tomas.carnecky@gmail.com>
2015-08-10 21:21:34 +0200

    Use css-syntax to process stylesheets

--- src/Data/Mole/Builder/Stylesheet.hs
+++ src/Data/Mole/Builder/Stylesheet.hs
@@ -1,5 +1,8 @@
 module Data.Mole.Builder.Stylesheet where
 
+
+import           Control.Monad
+
 import           Data.Map (Map)
 
 import qualified Data.Map as M
@@ -7,38 +10,43 @@ import qualified Data.Set as S
 
 import           Data.ByteString.Char8 (pack)
 
-import qualified Data.Text as T
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
 
 import           Data.Maybe
 
 import           Data.Mole.Types
 import           Data.Mole.Builder.Internal.Fingerprint
-import           Data.Mole.Builder.Internal.Template
+
+import           Data.CSS.Syntax.Tokens
 
 
 
 stylesheetBuilder :: String -> String -> Handle -> AssetId -> IO Builder
 stylesheetBuilder pubId src _ _ = do
-    body <- readFile src
+    body <- T.readFile src
 
-    let t@(Template fragments) = template body
-    let deps = catMaybes $ (flip map) fragments $ \f ->
-            case f of
-                (Lit _) -> Nothing
-                (Var x) -> Just $ AssetId (T.unpack x)
+    let Right tokens = tokenize body
+    let deps = catMaybes $ (flip map) tokens $ \t ->
+            case t of
+                (Url x) -> Just $ AssetId (T.unpack x)
+                _       -> Nothing
 
     return $ Builder
         { assetSources      = S.singleton src
         , assetDependencies = S.fromList deps
-        , packageAsset      = r t
+        , packageAsset      = r tokens
         }
 
   where
-    r :: Template -> Map AssetId String -> Either Error Result
-    r t m = do
-        body <- render t $ \x -> do
-            case M.lookup (AssetId (T.unpack x)) m of
+    r :: [Token] -> Map AssetId String -> Either Error Result
+    r tokens m = do
+        newTokens <- forM tokens $ \t -> case t of
+            (Url x) -> case M.lookup (AssetId (T.unpack x)) m of
                 Nothing -> Left (UndeclaredDependency (AssetId (T.unpack x)))
-                Just v -> Right $ T.pack v
+                Just v -> Right (Url $ T.pack v)
+            _ -> return t
+
+        let body = T.unpack $ serialize newTokens
 
         return $ Result (fingerprint (pack body) pubId) $ Just (pack body, "text/css")
