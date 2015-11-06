233061dada61e132af42410db88aa8d8cb2a1e4f
Tomas Carnecky <tomas.carnecky@gmail.com>
2015-11-06 23:33:08 +0100

    Comment on externalBuilder

--- src/Data/Mole/Builder/External.hs
+++ src/Data/Mole/Builder/External.hs
@@ -8,6 +8,15 @@ import           Data.Mole.Types
 
 
 
+-- | The external builder is used to replace 'AssetId's with a constant.
+--
+-- One use case is if you want to insert a version number or git revision
+-- in a HTML or JavaScript file:
+--
+--    <meta name="version" content="VERSION" />
+--
+-- and then add @--asset VERSION=`git rev-parse HEAD`@ to the command line.
+
 externalBuilder :: PublicIdentifier -> Handle -> AssetId -> IO Builder
 externalBuilder pubId _ _ = do
     return $ Builder
